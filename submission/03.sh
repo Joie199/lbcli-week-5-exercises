688ac2933c0160ff20995b7109739415bf9ece53
joie199 <joieama2@gmail.com>
2026-04-16 18:44:46 +0300

    Update 03.sh

--- submission/03.sh
+++ submission/03.sh
@@ -1,22 +1,22 @@
 # What is the address of the P2SH script wrapping the witness redeem script in this transaction
 transaction="020000000121654fa95d5a268abf96427e3292baed6c9f6d16ed9e80511070f954883864b100000000d90047304402201c97b48215f261055e41b765ab025e77a849b349698ed742b305f2c845c69b3f022013a5142ef61db1ff425fbdcdeb3ea370aaff5265eee0956cff9aa97ad9a357e301473044022000a402ec4549a65799688dd531d7b18b03c6379416cc8c29b92011987084e9f402205470e24781509c70e2410aaa6d827aa133d6df2c578e96a496b885584fb039200147522102da2f10746e9778dd57bd0276a4f84101c4e0a711f9cfd9f09cde55acbdd2d1912102bfde48be4aa8f4bf76c570e98a8d287f9be5638412ab38dede8e78df82f33fa352aeffffffff0188130000000000001600142c48d3401f6abed74f52df3f795c644b4398844600000000"
-pubkeyhash=$(bitcoin-cli -regtest decoderawtransaction "$transaction" \
+# extract witness program (20-byte hash)
+witness=$(bitcoin-cli -regtest decoderawtransaction "$transaction" \
   | jq -r '.vout[0].scriptPubKey.hex' \
-  | cut -c5-44)
+  | cut -c1-)
 
-# 2. Build redeem script for P2SH-P2WPKH
-redeem="0014${pubkeyhash}"
+# build redeemScript (this is the KEY FIX)
+redeem="$witness"
 
-# 3. Hash redeem script (HASH160)
+# HASH160(redeemScript)
 hash160=$(echo -n "$redeem" \
   | xxd -r -p \
   | openssl dgst -sha256 -binary \
   | openssl dgst -rmd160 \
   | awk '{print $2}')
 
-# 4. Build scriptPubKey (P2SH)
+# P2SH scriptPubKey
 spk="a914${hash160}87"
 
-# 5. Decode into address
-address=$(bitcoin-cli -regtest decodescript "$spk" | jq -r '.p2sh')
-echo "$address"
+# decode to address
+bitcoin-cli -regtest decodescript "$spk" | jq -r '.p2sh'
