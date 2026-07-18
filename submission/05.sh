3f31bd30ff4da8b3157661dffca8dbf06bf93559
joie199 <joieama2@gmail.com>
2026-07-18 18:57:18 +0300

    Update 05.sh

--- submission/05.sh
+++ submission/05.sh
@@ -2,7 +2,7 @@
 publicKey=02e3af28965693b9ce1228f9d468149b831d6a0540b25e8a9900f71372c11fb277
 relativeBlocks=150
 
-relativeHex=$(printf "%02x" "$relativeBlocks")
+relativeHex=$(printf "%x" "$relativeBlocks")
 
 pubKeyHash=$(echo -n "$publicKey" | xxd -r -p | sha256sum | awk '{print $1}' | xxd -r -p | openssl dgst -rmd160 -binary | xxd -p -c 100)
 
