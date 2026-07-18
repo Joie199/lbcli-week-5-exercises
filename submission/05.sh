64f111e8676bc740e74badb7d064f98d2e80b4b4
joie199 <joieama2@gmail.com>
2026-07-18 18:28:23 +0300

    Update 05.sh

--- submission/05.sh
+++ submission/05.sh
@@ -2,10 +2,18 @@
 publicKey=02e3af28965693b9ce1228f9d468149b831d6a0540b25e8a9900f71372c11fb277
 relativeBlocks=150
 
-relativeHex=$(printf "%02x" "$relativeBlocks")
+$relativeHex = "{0:x2}" -f $relativeBlocks
 
-pubKeyHash=$(echo -n "$publicKey" | xxd -r -p | openssl dgst -sha256 -binary | openssl dgst -rmd160 -binary | xxd -p -c 100)
+$pubKeyBytes = [System.Convert]::FromHexString($publicKey)
 
-redeemScript="01${relativeHex}b27576a914${pubKeyHash}88ac"
+$sha256 = [System.Security.Cryptography.SHA256]::Create()
+$sha256Hash = $sha256.ComputeHash($pubKeyBytes)
 
-echo "$redeemScript"
+$ripemd160 = [System.Security.Cryptography.RIPEMD160]::Create()
+$pubKeyHash = $ripemd160.ComputeHash($sha256Hash)
+
+$pubKeyHashHex = ($pubKeyHash | ForEach-Object { $_.ToString("x2") }) -join ""
+
+$redeemScript = "01$relativeHex" + "b27576a914$pubKeyHashHex" + "88ac"
+
+echo $redeemScript
