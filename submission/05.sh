f55f429a3350b06bd7062dac48edca4ff08ad8ff
joie199 <joieama2@gmail.com>
2026-07-18 18:30:44 +0300

    Update 05.sh

--- submission/05.sh
+++ submission/05.sh
@@ -2,18 +2,18 @@
 publicKey=02e3af28965693b9ce1228f9d468149b831d6a0540b25e8a9900f71372c11fb277
 relativeBlocks=150
 
-$relativeHex = "{0:x2}" -f $relativeBlocks
+$relativeHex="{0:x2}" -f $relativeBlocks
 
-$pubKeyBytes = [System.Convert]::FromHexString($publicKey)
+$pubKeyBytes=[System.Convert]::FromHexString($publicKey)
 
-$sha256 = [System.Security.Cryptography.SHA256]::Create()
-$sha256Hash = $sha256.ComputeHash($pubKeyBytes)
+$sha256=[System.Security.Cryptography.SHA256]::Create()
+$sha256Hash=$sha256.ComputeHash($pubKeyBytes)
 
-$ripemd160 = [System.Security.Cryptography.RIPEMD160]::Create()
-$pubKeyHash = $ripemd160.ComputeHash($sha256Hash)
+$ripemd160=[System.Security.Cryptography.RIPEMD160]::Create()
+$pubKeyHash=$ripemd160.ComputeHash($sha256Hash)
 
-$pubKeyHashHex = ($pubKeyHash | ForEach-Object { $_.ToString("x2") }) -join ""
+$pubKeyHashHex=($pubKeyHash | ForEach-Object { $_.ToString("x2") }) -join ""
 
-$redeemScript = "01$relativeHex" + "b27576a914$pubKeyHashHex" + "88ac"
+$redeemScript="01$relativeHex" + "b27576a914$pubKeyHashHex" + "88ac"
 
 echo $redeemScript
